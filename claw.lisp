a0d6eb01fede8168289b2d4414d63f32215367e8
Pavel Korolev <dev@borodust.org>
2018-01-17 09:18:49 +0300

    Update :claw library descriptor

--- claw.lisp
+++ claw.lisp
@@ -1,12 +1,13 @@
 (claw:c-include "bodge_nanovg.h" :bodge-nanovg
-  :package :%nanovg
+  :in-package :%nanovg
   :sysincludes (:nanovg-lib)
   :include-sources ("nanovg.h" "nanovg_gl.h")
-  :symbol-exceptions (("GLuint" gl-uint)
-                      ("GLuint64" gl-uint64)
-                      ("nvgRGBf" rgb-f)
-                      ("nvgRGBAf" rgba-f))
-  :symbol-prefix ("nvgl_" "NVGL_"
-                          "nvgl" "NVGL"
-                          "nvg_" "NVG_"
-                          "nvg" "NVG"))
+  :rename-symbols (claw:in-pipeline
+                   (claw:by-changing "GLuint" 'gl-uint)
+                   (claw:by-changing"GLuint64" 'gl-uint64)
+                   (claw:by-changing "nvgRGBf" 'rgb-f)
+                   (claw:by-changing "nvgRGBAf" 'rgba-f)
+                   (claw:by-removing-prefixes "nvgl_" "NVGL_"
+                                              "nvgl" "NVGL"
+                                              "nvg_" "NVG_"
+                                              "nvg" "NVG")))
