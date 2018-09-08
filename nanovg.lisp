5efe1dab8bc0f720298e25e9bb5497e58fddfaf7
Pavel Korolev <dev@borodust.org>
2018-09-08 21:33:02 +0300

    Remove old-specs

--- nanovg.lisp
+++ nanovg.lisp
@@ -11,11 +11,17 @@
 
 
 (defun make-context (&rest create-flags)
-  (%nvg:create-gl3 (apply #'mask 'create-flags create-flags)))
+  #-bodge-gl2
+  (%nvg:create-gl3 (apply #'mask 'create-flags create-flags))
+  #+bodge-gl2
+  (%nvg:create-gl2 (apply #'mask 'create-flags create-flags)))
 
 
 (defun destroy-context (context)
-  (%nvg:delete-gl3 context))
+  #-bodge-gl2
+  (%nvg:delete-gl3 context)
+  #+bodge-gl2
+  (%nvg:delete-gl2 context))
 
 
 (defun make-rgba-image (context width height data &rest flags)
